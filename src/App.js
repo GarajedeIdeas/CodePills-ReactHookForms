6962d783fd8822cffd82de2e215ca2bc17e05e01
Garaje de Ideas <82031166+GarajedeIdeas@users.noreply.github.com>
2022-03-25 09:09:07 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Formulario from './components/Formulario';
+
+function App() {
+  return (
+    <div className="App">
+      <Formulario />
+    </div>
+  );
+}
+
+export default App;
